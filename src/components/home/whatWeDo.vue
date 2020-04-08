0ae51d53c711c71ceb7d4dc42a56f3680da36de5
aleazofeifam <aleazofeifam@gmail.com>
2020-04-08 12:14:25 -0600

    Whats here for me ribbon updated

--- src/components/home/whatWeDo.vue
+++ src/components/home/whatWeDo.vue
@@ -2,10 +2,10 @@
     <v-container class="pa-0">
         <v-layout wrap align-center justify-center row fill-height >
            <v-flex xs12 md3 lg3 class="pa-2">
-               <p class="google-font" style="font-size:180%">What we do?</p>
-               <p class="google-font" style="font-size:110%">Google Developer Groups (GDGs) are for developers who are interested in Google's developer technology.</p>
+               <p class="google-font" style="font-size:180%">What is here for me?</p>
+               <p class="google-font" style="font-size:110%">TODO Google Developer Groups (GDGs) are for developers who are interested in Google's developer technology.</p>
 
-               <p class="google-font" style="font-size:110%">About different Google technologies</p>
+               <p class="google-font" style="font-size:110%">About different AAD Products</p>
 
                 <span v-for="(item,i) in whatWeDoData" :key="i">
                     <v-tooltip bottom>
@@ -28,27 +28,32 @@
                 <v-layout row wrap>
 
                     <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
-                        <v-icon large style="font-size:300%">chat_bubble</v-icon>
-                        <p class="google-font" style="font-size:130%">Talks</p>
-                        <p class="google-font" style="font-size:110%">Get updated with the latest news and announcements</p>
+                        <i large style="font-size:300%" class="fas fa-terminal"></i>
+                        <p> </p>
+                        <p class="google-font" style="font-size:130%">AZ CLI</p>
+                        <p class="google-font" style="font-size:110%">Learn how to use it to interact with your resources directly from your command line</p>
                     </v-flex>
 
                     <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
-                        <v-icon large style="font-size:300%">code</v-icon>
-                        <p class="google-font" style="font-size:130%">Codelabs</p>
-                        <p class="google-font" style="font-size:110%">Get hands-on experience and guidance from the community members</p>
+                   
+                        <i large style="font-size:300%" class="fas fa-code-branch"></i>
+                        <p> </p>
+                        <p class="google-font" style="font-size:130%">APIs</p>
+                        <p class="google-font" style="font-size:110%">Microsoft have multiple APIs we will learn how to perform multiple operations that can't be done from the Portal </p>
                     </v-flex>
 
                     <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
-                        <v-icon large style="font-size:300%">school</v-icon>
-                        <p class="google-font" style="font-size:130%">Campus Roadshows</p>
-                        <p class="google-font" style="font-size:110%">Share knowledge in different Companies, colleges and universities</p>
+                        <i style="font-size:300%" class="fas fa-laptop-code"></i>
+                        <p> </p>
+                        <p class="google-font" style="font-size:130%">PortalUX</p>
+                        <p class="google-font" style="font-size:110%">Take the most out of the Portal, learn some tips and tricks</p>
                     </v-flex>
 
                     <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
-                        <v-icon large style="font-size:300%">airplay</v-icon>
-                        <p class="google-font" style="font-size:130%">Live Viewing Parties</p>
-                        <p class="google-font" style="font-size:110%">Share knowledge in different Companies, colleges and universities</p>
+                        <i style="font-size:300%" class="fas fa-graduation-cap"></i>
+                         <p> </p>
+                        <p class="google-font" style="font-size:130%">Learning Material</p>
+                        <p class="google-font" style="font-size:110%">The cloud business is constantly growing, you will learn how to be always updated </p>
                     </v-flex>
 
                 </v-layout>
