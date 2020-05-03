1d9c92d47294ba6342d2322e640fbb5e9ce91908
aleazofeifam <aleazofeifam@gmail.com>
2020-05-02 19:26:55 -0600

    Update the Home Screen

--- src/components/home/whatWeDo.vue
+++ src/components/home/whatWeDo.vue
@@ -30,7 +30,7 @@
                         <i large style="font-size:300%" class="fas fa-user-times"></i>
                         <p> </p>
                         <p class="google-font" style="font-size:130%">Access Management</p>
-                        <p class="google-font" style="font-size:110%">Learn how to use it to interact with your resources directly from your command line</p>
+                        <p class="google-font" style="font-size:110%">TODO Need to do update </p>
                     </v-flex>
 
                     <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
@@ -38,14 +38,14 @@
                         <i large style="font-size:300%" class="fas fa-user-cog"></i>
                         <p> </p>
                         <p class="google-font" style="font-size:130%">Identity Management</p>
-                        <p class="google-font" style="font-size:110%">Microsoft have multiple APIs we will learn how to perform multiple operations that can't be done from the Portal </p>
+                        <p class="google-font" style="font-size:110%">TODO Need to do update </p>
                     </v-flex>
 
                     <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
                         <i style="font-size:300%" class="fas fa-user-shield"></i>
                         <p> </p>
                         <p class="google-font" style="font-size:130%">Identity Governance</p>
-                        <p class="google-font" style="font-size:110%">Take the most out of the Portal, learn some tips and tricks</p>
+                        <p class="google-font" style="font-size:110%">TODO Need to do update </p>
                     </v-flex>
 
                     <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
